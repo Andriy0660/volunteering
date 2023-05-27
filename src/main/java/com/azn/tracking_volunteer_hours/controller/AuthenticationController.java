636ba2a5eb7ef2dd8950eaa9bb34bdbcf75752aa
Norbert <d4zershow@gmail.com>
2023-05-27 15:30:25 +0300

    Initialization

---
+++ src/main/java/com/azn/tracking_volunteer_hours/controller/AuthenticationController.java
@@ -0,0 +1,17 @@
+package com.azn.tracking_volunteer_hours.controller;
+
+
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Validated
+public class AuthenticationController {
+
+}
