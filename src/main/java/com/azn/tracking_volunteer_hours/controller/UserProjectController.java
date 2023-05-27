875b9b162bb642f90908cb36bca22e6467a9a02a
Norbert <d4zershow@gmail.com>
2023-05-27 17:54:03 +0300

    Initialization

--- src/main/java/com/azn/tracking_volunteer_hours/controller/UserProjectController.java
+++ src/main/java/com/azn/tracking_volunteer_hours/controller/UserProjectController.java
@@ -1,8 +1,11 @@
 package com.azn.tracking_volunteer_hours.controller;
 
+import com.azn.tracking_volunteer_hours.entity.Project;
 import com.azn.tracking_volunteer_hours.entity.User;
 import com.azn.tracking_volunteer_hours.entity.UserDetailsImpl;
 import com.azn.tracking_volunteer_hours.entity.UserProject;
+import com.azn.tracking_volunteer_hours.exception.BadRequestException;
+import com.azn.tracking_volunteer_hours.service.ProjectService;
 import com.azn.tracking_volunteer_hours.service.UserProjectsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +19,8 @@ import java.time.LocalDateTime;
 @RestController
 @RequiredArgsConstructor
 public class UserProjectController {
-    private final UserProjectsService service;
+    private final UserProjectsService userProjectService;
+    private final ProjectService projectService;
     @PostMapping("/contributeHours")
     public ResponseEntity<Void> contributeHours(
             @RequestParam("hour") int hours,
@@ -26,13 +30,18 @@ public class UserProjectController {
                 getAuthentication().getPrincipal();
         User user = userDetails.getUser();
 
+        Project project = projectService.findProjectById(project_id);
+        if(project.getStartTime().isAfter(LocalDateTime.now())){
+            throw new BadRequestException("This project didn't start yet");
+        }
+
         UserProject userProject = UserProject.builder()
                 .date(LocalDateTime.now())
                 .hours(hours)
                 .user_id(user.getId())
                 .project_id(project_id)
                 .build();
-        service.save(userProject);
+        userProjectService.save(userProject);
 
         return ResponseEntity.ok().build();
     }
