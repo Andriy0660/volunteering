284ac77f5be32ebdf7f806ab5cdf7264e0fa5f3c
Norbert <d4zershow@gmail.com>
2023-05-27 14:42:15 +0300

    Initialization

---
+++ src/main/java/com/azn/tracking_volunteer_hours/TrackingVolunteerHoursApplication.java
@@ -0,0 +1,13 @@
+package com.azn.tracking_volunteer_hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingVolunteerHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingVolunteerHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azn/tracking_volunteer_hours/TrackingVolunteerHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azn.tracking_volunteer_hours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingVolunteerHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
