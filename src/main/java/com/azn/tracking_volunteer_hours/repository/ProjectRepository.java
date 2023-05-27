b17a802970dae048b8a5b4e81cbc063b0843bf50
Zakhar <limouoy@gmail.com>
2023-05-27 17:08:50 +0300

    zakhar

--- src/main/java/com/azn/tracking_volunteer_hours/repository/ProjectRepository.java
+++ src/main/java/com/azn/tracking_volunteer_hours/repository/ProjectRepository.java
@@ -3,6 +3,10 @@ package com.azn.tracking_volunteer_hours.repository;
 import com.azn.tracking_volunteer_hours.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<Project,Long> {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+public List<Project> findAllByStartTimeIsBefore(LocalDateTime now);
+public List<Project> findAllByStartTimeIsAfter(LocalDateTime now);
 }
