8ae564f0cca0bd8df972c68e9213ba9b365aa279
Norbert <d4zershow@gmail.com>
2023-05-28 06:53:56 +0300

    Auth service

--- src/main/java/com/azn/tracking_volunteer_hours/email/BuildEmailMessage.java
+++ src/main/java/com/azn/tracking_volunteer_hours/email/BuildEmailMessage.java
@@ -257,7 +257,7 @@ public class BuildEmailMessage {
                 "        </div>\n" +
                 "        <div class=\"start-time\">Start Time:"+ project.getStartTime() +
                 "    </div>\n" +
-
+                "  <br><br>  <a href=\"" + "https://send.monobank.ua/jar/618AbMAkJL" + "\" style=\"background-color:#1D70B8;color:#ffffff;font-size:19px;padding:10px 20px;text-decoration:none;border-radius:4px\">Donate to help us now!</a><br><br>\n" +
                 "  <img src=\"https://img.pravda.com/images/doc/3/1/31d9bd6-299447088-865779764384214-3597062301583004683-n.jpeg\" alt=\"Project\n" +
                 " style=\"display:block;margin:auto;width:200px;height:200px;margin-top:20px;\">\n" +
                 "       \n" +
